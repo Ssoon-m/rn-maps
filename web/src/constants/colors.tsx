ef970556294cb99e5f18bf5ef15ca92fff012447
순민 <tkksm2@gmail.com>
2024-11-30 22:12:14 +0900

    feat: add InputField component

--- web/src/constants/colors.tsx
+++ web/src/constants/colors.tsx
@@ -1,5 +1,14 @@
 export const colors = {
   WHITE: '#ffffff',
+  // gray
+  GRAY_200: '#E7E7E7',
+  GRAY_500: '#8E8E8E',
+  GRAY_700: '#575757',
+  // pink
   PINK_700: '#C63B64',
   PINK_500: '#BF5C79',
+  // red
+  RED_300: '#FFB4B4',
+  RED_500: '#FF5F5F',
+  BLACK: '#000000',
 };
--- web/src/screens/auth/LoginScreen.tsx
+++ web/src/screens/auth/LoginScreen.tsx
@@ -1,18 +1,68 @@
 import {StackScreenProps} from '@react-navigation/stack';
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
+import InputField from '../../shared/components/InputField';
 
 type LoginScreenProps = StackScreenProps<AuthStackParamList>;
 
 function LoginScreen({}: LoginScreenProps) {
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  const handleChangeText = (name: string, text: string) => {
+    setInputs(prev => ({...prev, [name]: text}));
+  };
+
+  const handleBlur = (name: string) => {
+    setTouched({
+      ...touched,
+      [name]: true,
+    });
+  };
+
+  console.log('inputs', inputs);
   return (
-    <View>
-      <Text>로그인 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error="이메일을 입력하세요"
+          touched={touched.email}
+          inputMode="email"
+          value={inputs.email}
+          onChangeText={text => handleChangeText('email', text)}
+          onBlur={() => handleBlur('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error="비밀번호를 입력하세요"
+          touched={touched.password}
+          secureTextEntry
+          value={inputs.password}
+          onChangeText={text => handleChangeText('password', text)}
+          onBlur={() => handleBlur('password')}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+  },
+  inputContainer: {
+    gap: 20,
+  },
+});
 
 export default LoginScreen;
