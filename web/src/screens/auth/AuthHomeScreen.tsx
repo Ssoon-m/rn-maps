221ddb634dca3e556b604455feedff8c444d9535
순민 <tkksm2@gmail.com>
2024-11-30 20:51:29 +0900

    feat: AuthHomeScreen

--- web/src/screens/auth/AuthHomeScreen.tsx
+++ web/src/screens/auth/AuthHomeScreen.tsx
@@ -1,6 +1,6 @@
 import {StackScreenProps} from '@react-navigation/stack';
 import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Dimensions, Image, SafeAreaView, StyleSheet, View} from 'react-native';
 import {AuthStackParamList} from '../../navigations/stack/AuthStackNavigator';
 import {authNavigations} from '../../constants/navigations';
 import Button from '../../shared/components/Button';
@@ -9,8 +9,15 @@ type AuthHomeScreenProps = StackScreenProps<AuthStackParamList>;
 
 function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image
+          resizeMode="contain"
+          style={styles.image}
+          source={require('../../assets/matzip.png')}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
         <Button
           variant="filled"
           label="로그인하기"
@@ -27,6 +34,25 @@ function AuthHomeScreen({navigation}: AuthHomeScreenProps) {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 30,
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  imageContainer: {
+    flex: 1.5,
+    width: Dimensions.get('screen').width / 2,
+  },
+  buttonContainer: {
+    flex: 1,
+    gap: 10,
+    width: '100%',
+  },
+});
 
 export default AuthHomeScreen;
