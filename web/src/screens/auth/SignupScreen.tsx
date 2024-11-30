6b8d97568aab99f818e9fe6eb5c0ade8e245b5d1
순민 <tkksm2@gmail.com>
2024-11-30 23:02:11 +0900

    feat: SignupScreen

--- web/src/screens/auth/SignupScreen.tsx
+++ web/src/screens/auth/SignupScreen.tsx
@@ -1,16 +1,52 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import InputField from '../../shared/components/InputField';
+import {useForm} from '../../shared/hooks/useForm';
+import Button from '../../shared/components/Button';
+import {validateSignup} from '../../shared/utils';
 
-interface SignupScreenProps {}
-
-function SignupScreen({}: SignupScreenProps) {
+function SignupScreen() {
+  const signup = useForm({
+    initialValue: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+    validate: validateSignup,
+  });
   return (
-    <View>
-      <Text>회원가입 스크린</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputContainer}>
+        <InputField
+          placeholder="이메일"
+          error={signup.errors.email}
+          touched={signup.touched.email}
+          inputMode="email"
+          {...signup.getTextInputProps('email')}
+        />
+        <InputField
+          placeholder="비밀번호"
+          error={signup.errors.password}
+          touched={signup.touched.password}
+          secureTextEntry
+          {...signup.getTextInputProps('password')}
+        />
+        <InputField
+          placeholder="비밀번호 확인"
+          error={signup.errors.passwordConfirm}
+          touched={signup.touched.passwordConfirm}
+          secureTextEntry
+          {...signup.getTextInputProps('passwordConfirm')}
+        />
+      </View>
+      <Button label="회원가입" />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {flex: 1, margin: 30},
+  inputContainer: {gap: 20, marginBottom: 30},
+});
 
 export default SignupScreen;
--- web/src/shared/utils/validate.ts
+++ web/src/shared/utils/validate.ts
@@ -3,18 +3,39 @@ type UserInfo = {
   password: string;
 };
 
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordValidate = (text: string) => text.length >= 8 && text.length < 20;
+
 const validateLogin = (values: UserInfo) => {
   const errors = {
     email: '',
     password: '',
   };
-  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+  if (!emailRegex.test(values.email)) {
     errors.email = '올바른 이메일 형식이 아닙니다.';
   }
-  if (values.password.length >= 8 && values.password.length < 20) {
+  if (!passwordValidate(values.password)) {
     errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
   }
   return errors;
 };
 
-export {validateLogin};
+const validateSignup = (values: UserInfo & {passwordConfirm: string}) => {
+  const errors = {
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  };
+  if (!emailRegex.test(values.email)) {
+    errors.email = '올바른 이메일 형식이 아닙니다.';
+  }
+  if (!passwordValidate(values.password)) {
+    errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
+  }
+  if (values.password !== values.passwordConfirm) {
+    errors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
+  }
+  return errors;
+};
+
+export {validateLogin, validateSignup};
